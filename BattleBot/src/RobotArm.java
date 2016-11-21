5060d59bcb39c996d716151537a0fa18c2a2da4c
benegoff <benegoff@gmail.com>
2016-11-21 11:52:24 -0700

    Added isClosed logic to Arm

--- BattleBot/src/RobotArm.java
+++ BattleBot/src/RobotArm.java
@@ -3,16 +3,24 @@ import lejos.nxt.NXTRegulatedMotor;
 public class RobotArm {
 
 	private NXTRegulatedMotor motor;
+	private boolean isClosed;
 	public RobotArm(NXTRegulatedMotor m){
+		isClosed = false;
 		motor = m;
 	}
 	
 	public void close(){
 		motor.rotate(60);
+		isClosed = true;
 	}
 	
 	public void open(){
 		motor.rotate(-60);
+		isClosed = false;
+	}
+	
+	public boolean isClosed(){
+		return isClosed;
 	}
 	
 }
